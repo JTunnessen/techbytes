307b65cacfd7ff4432cb80a1fe0dd2ebf2d249dd
jimt2 <jtunnessen@gmail.com>
2016-05-31 00:33:34 -0400

    Added Tech Blog

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531021129) do
+ActiveRecord::Schema.define(version: 20160531041018) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "tags"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "videos", force: :cascade do |t|
     t.string   "title"
